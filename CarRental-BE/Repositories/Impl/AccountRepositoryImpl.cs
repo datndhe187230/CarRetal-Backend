683177024fe15e1a2e2c87228bec28de04ef9a27
kiennguyen <kienduccml@gmail.com>
2025-06-04 11:52:12 +0700

    Establish Global Exception Handling

--- CarRental-BE/Repositories/Impl/AccountRepositoryImpl.cs
+++ CarRental-BE/Repositories/Impl/AccountRepositoryImpl.cs
@@ -1,24 +1,31 @@
 ﻿using CarRental_BE.Data;
+using CarRental_BE.Exceptions;
 using CarRental_BE.Models.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace CarRental_BE.Repositories.Impl
 {
-    public class AccountRepository : IAccountRepository
+    public class AccountRepositoryImpl : IAccountRepository
     {
         private readonly CarRentalContext _carRentalContext;
 
-        public AccountRepository(CarRentalContext carRentalContext)
+        public AccountRepositoryImpl(CarRentalContext carRentalContext)
         {
             _carRentalContext = carRentalContext;
         }
 
         public async Task<Account?> getAccountByEmailWithRole(string email)
         {
-            return await _carRentalContext.Accounts
-                                      .Include(a => a.Role)
-                                      .FirstOrDefaultAsync(a => a.Email == email);
+            var account = await _carRentalContext.Accounts
+                .Include(a => a.Role)
+                .FirstOrDefaultAsync(a => a.Email == email);
+
+            if (account == null)
+                throw new UserNotFoundException(email);
+
+            return account;
         }
+
 
         public async Task<Account?> GetAccountById(Guid id)
         {
