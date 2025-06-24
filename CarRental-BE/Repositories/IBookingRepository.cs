04b608e22d73c041d09f2c4a3f6f69601f211752
kiennguyen <kienduccml@gmail.com>
2025-06-25 06:14:58 +0700

    Add dashboard Statistics

--- CarRental-BE/Repositories/IBookingRepository.cs
+++ CarRental-BE/Repositories/IBookingRepository.cs
@@ -1,13 +1,22 @@
 ﻿using CarRental_BE.Models.DTO;
 using CarRental_BE.Models.Entities;
+using CarRental_BE.Models.VO.Statistic;
 
 public interface IBookingRepository
 {
     Task<List<Booking>> GetAllBookingsAsync();
     Task<List<Booking>> GetBookingsByAccountIdAsync(Guid accountId);
     Task<(List<Booking>, int)> GetBookingsWithPagingAsync(int page, int pageSize);
-
     Task<Booking> GetBookingByBookingIdAsync(string id);
     Task<Booking?> UpdateBookingAsync(string bookingNumber, BookingEditDTO bookingDto);
+    Task<IEnumerable<Booking>> GetRecentBookingsAsync(int count = 10);
+
+
+    Task<int> GetTotalBookingsCountAsync();
+    Task<int> GetActiveBookingsCountAsync();
+    Task<decimal> GetTotalRevenueAsync();
+    Task<IEnumerable<MonthlyRevenueVO>> GetMonthlyRevenueAsync(int year);
+    Task<IEnumerable<TopBookedVehicleVO>> GetTopBookedVehiclesAsync(int count = 5);
+    Task<IEnumerable<BookingStatusCountVO>> GetBookingStatusCountsAsync();
 
 }
