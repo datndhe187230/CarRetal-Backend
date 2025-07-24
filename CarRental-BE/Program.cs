18ae0aa36ea40a8f1d04e6391dfbf6789f3acf86
HaloicCa <hungnvhe187005@fpt.edu.vn>
2025-07-24 23:18:22 +0700

    update func

--- CarRental-BE/Program.cs
+++ CarRental-BE/Program.cs
@@ -53,6 +53,8 @@ builder.Services.AddScoped<ITransactionRepository, TransactionRepositoryImpl>();
 builder.Services.AddScoped<IDashboardService, DashboardServiceImpl>();
 builder.Services.AddScoped<IWalletRepository, WalletRepositoryImpl>();
 builder.Services.AddScoped<IWalletService, WalletServiceImpl>();
+builder.Services.AddScoped<IFeedbackRepository, FeedbackRepository>();
+builder.Services.AddScoped<IFeedbackService, FeedbackServiceImpl>();
 
 //Configure Elasticsearch settings (local)
 var settings = new ElasticsearchClientSettings(new Uri("https://localhost:9200"))
